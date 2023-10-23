5acfb20cb5507b1ad3d12f00fce998538de33ef9
Ishwari Kape <ishwarikape117@gmail.com>
2023-10-23 09:54:16 -0700

    Added Comments in JS

---
+++ comment.js
@@ -0,0 +1,10 @@
+
+
+document.write("there are two types of Comments");
+document.write("1.single Line comment ");
+//This is single line Comment
+
+document.write("2.Multiline comment")
+/*This is 
+            Multiline comment*/
+
